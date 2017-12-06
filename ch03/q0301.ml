3001e8a5738fa0bc3173e39d97a850a40eddd07c
sky0621 <sky14work@gmail.com>
2017-12-07 01:45:56 +0900

    continue

---
+++ ch03/q0301.ml
@@ -0,0 +1,10 @@
+(* C:\FHS\usr\local\src\ocaml\github.com\sky0621\study-ocaml\ch03\q0301.ml *)
+
+let pi = 3.1415 ;;
+
+pi ;;
+
+let jikyu = 850 ;;
+
+(25 * jikyu) + (28 * jikyu) + (31 * jikyu) ;;
+
