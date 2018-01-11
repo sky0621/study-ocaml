3d214fcfe589466e46fa8d8052eb525fc48291f0
sky0621 <sky14work@gmail.com>
2018-01-12 02:29:58 +0900

    re ch08

---
+++ re/ch08.ml
@@ -0,0 +1,26 @@
+(* C:\FHS\usr\local\src\ocaml\github.com\sky0621\study-ocaml\re\ch08.ml *)
+
+(* Syntax Error or Unbound Value
+tsuuchi {namae="asai"; tensuu=70; seiseki"B"};;
+ *)
+
+(* Syntax Error or Unbound Value
+tsuuchi {namae="asai"; tensuu=70; seiseki="B"};;
+ *)
+
+(* Syntax Error or Unbound Value
+tsuuchi {namae="asai"; tensuu=70; seiseki="B"};;
+ *)
+
+(* Syntax Error or Unbound Value
+tsuuchi {namae="asai"; tensuu=70; seiseki="B"};;
+ *)
+
+(* Syntax Error or Unbound Value
+val# tsuuchi {namae="asai"; tensuu=70; seiseki="B"};;
+ *)
+
+(* Syntax Error or Unbound Value
+val tsuuchi {namae="asai"; tensuu=70; seiseki="B"};;
+ *)
+
