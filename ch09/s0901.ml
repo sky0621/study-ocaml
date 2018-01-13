2518467e3c70b91de4240af50e294c464bf5629f
sky0621 <sky14work@gmail.com>
2018-01-14 02:05:22 +0900

    ch09

---
+++ ch09/s0901.ml
@@ -0,0 +1,15 @@
+(* C:\FHS\usr\local\src\ocaml\github.com\sky0621\study-ocaml\ch09\s0901.ml *)
+
+[]
+  ;;
+
+(* Syntax Error or Unbound Value
+{};;
+ *)
+
+"Sunday" :: "Monday" :: "Tuesday" :: "Wednesday" :: "Thursday" :: "Friday" :: "Saturday" :: [];;
+
+[1; 2; 3];;
+
+1 :: [2; 3];;
+
